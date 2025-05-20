0116d7f816c49a886d4db2896ab24a3f8cca16fc
Pedro <pedro.paula.senai@gmail.com>
2025-05-20 13:49:14 -0300

    changes

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-from google import genai
+import google.generativeai as genai
 import os
 from dotenv import load_dotenv 
 import json
